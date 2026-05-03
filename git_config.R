bf3950255eb02304979db3b8e23cf3cbfb826447
anders-d-s <andersdrejergaard@gmail.com>
2026-05-03 16:29:29 +0200

    .

--- git_config.R
+++
@@ -1,9 +0,0 @@
-library(usethis)
-create_github_token()
-
-library(gitcreds)
-gitcreds_set()
-
-#Git Bash commands to set user
-#git config --global user.email "andersdrejergaard@gmail.com"
-#git config --global user.name "anders-d-s"
