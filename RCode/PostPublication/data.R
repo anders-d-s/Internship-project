94b21a23362196a21e9410f076c34ab7755769fc
anders-d-s <andersdrejergaard@gmail.com>
2026-05-15 12:00:00 +0200

    .

---
+++ RCode/PostPublication/data.R
@@ -0,0 +1,164 @@
+################################################################################
+############################# Monthly data #####################################
+################################################################################
+
+#Import data
+data <- read.table(file = "Data/[dnk]_[all_factors]_[monthly]_[vw_cap].csv",
+                          sep = ",", dec = ".",
+                          header = TRUE,
+                          na.strings = "null")
+#Market return
+mkt <- read.table(file = "Data/[dnk]_[mkt]_[monthly]_[vw_cap].csv",
+                   sep = ",", dec = ".",
+                   header = TRUE,
+                   na.strings = "null")
+data <- rbind(data, mkt)
+rm(mkt)
+
+
+#Count number of different names
+names <- unique(data[,"name"])
+min(data$date)
+max(data$date)
+sorted_dates <- sort(unique(data[,"date"]), decreasing = FALSE)
+
+empty_date_dataframe <- data.frame(date = sorted_dates)
+# Start with the empty date frame
+loc_df <- empty_date_dataframe
+
+# Loop over all factor names
+for (nm in names) {
+  sub_df <- data[data$name == nm, c("date", "ret")]
+  
+  # Rename ret column to factor name
+  colnames(sub_df)[colnames(sub_df) == "ret"] <- nm
+  
+  # Merge into main dataframe
+  loc_df <- merge(loc_df, sub_df, by = "date", all = TRUE)
+}
+
+
+monthly_factors <- loc_df
+
+#Remove data before 1991-05-01 due to missing data (64 obs)
+monthly_factors <- monthly_factors[monthly_factors$date >= as.Date("1991-05-31"), ]
+row.names(monthly_factors) <- NULL
+
+#Remove factors with too many NA's
+monthly_factors <- monthly_factors[, setdiff(names(monthly_factors), c("iskew_ff3_21d", "ivol_ff3_21d", "resff3_6_1", "resff3_12_1"))]
+
+######################## Handle NA s###########################################
+#6 month rolling mean
+
+na_padding <- monthly_factors[rep(1,5), ]
+na_padding[,] <- NA
+
+# prepend it
+monthly_factors <- rbind(na_padding, monthly_factors)
+rownames(monthly_factors) <- NULL
+
+for (factor in names) {
+  
+  start_index <- which(!is.na(monthly_factors[[factor]]))[1]
+  end_index <- tail(which(!is.na(monthly_factors[[factor]])), 1)
+  
+  for (i in (start_index):(end_index-1)) {
+    
+    rolling_mean <- mean(monthly_factors[[factor]][(i-5):i], na.rm = TRUE)
+    
+    if (is.na(monthly_factors[[factor]][i+1])) {
+      monthly_factors[[factor]][i+1] <- rolling_mean
+    }
+  }
+}
+
+monthly_factors <- monthly_factors[-c(1:5),]
+rownames(monthly_factors) <- NULL
+
+# Save your data_list into an .RData file
+save(monthly_factors, file = "Data/monthly_factors.RData")
+
+################################################################################
+############################# Daily data #######################################
+################################################################################
+
+rm(list = ls())
+
+#Import data
+data <- read.table(file = "Data/[dnk]_[all_factors]_[daily]_[vw_cap].csv",
+                   sep = ",", dec = ".",
+                   header = TRUE,
+                   na.strings = "null")
+#Market return
+mkt <- read.table(file = "Data/[dnk]_[mkt]_[daily]_[vw_cap].csv",
+                  sep = ",", dec = ".",
+                  header = TRUE,
+                  na.strings = "null")
+data <- rbind(data, mkt)
+rm(mkt)
+
+
+#Count number of different names
+names <- unique(data[,"name"])
+min(data$date)
+max(data$date)
+sorted_dates <- sort(unique(data[,"date"]), decreasing = FALSE)
+
+empty_date_dataframe <- data.frame(date = sorted_dates)
+# Start with the empty date frame
+loc_df <- empty_date_dataframe
+
+# Loop over all factor names
+for (nm in names) {
+  sub_df <- data[data$name == nm, c("date", "ret")]
+  
+  # Rename ret column to factor name
+  colnames(sub_df)[colnames(sub_df) == "ret"] <- nm
+  
+  # Merge into main dataframe
+  loc_df <- merge(loc_df, sub_df, by = "date", all = TRUE)
+}
+
+
+daily_factors <- loc_df
+
+#Remove data before 1991-05-01 due to missing data
+daily_factors <- daily_factors[daily_factors$date >= as.Date("1991-05-01"), ]
+row.names(daily_factors) <- NULL
+
+#Remove factors with too many NA's
+daily_factors <- daily_factors[, setdiff(names(daily_factors),
+                                             c("iskew_ff3_21d", "ivol_ff3_21d", "resff3_6_1", "resff3_12_1"))]
+
+######################## Handle NA s###########################################
+#20 day rolling mean
+
+na_padding <- daily_factors[rep(1,19), ]
+na_padding[,] <- NA
+
+# prepend it
+daily_factors <- rbind(na_padding, daily_factors)
+rownames(daily_factors) <- NULL
+
+for (factor in names) {
+  
+  start_index <- which(!is.na(daily_factors[[factor]]))[1]
+  end_index <- tail(which(!is.na(daily_factors[[factor]])), 1)
+  
+  for (i in (start_index):(end_index-1)) {
+    
+    rolling_mean <- mean(daily_factors[[factor]][(i-19):i], na.rm = TRUE)
+    
+    if (is.na(daily_factors[[factor]][i+1])) {
+      daily_factors[[factor]][i+1] <- rolling_mean
+    }
+  }
+}
+
+daily_factors <- daily_factors[-c(1:19),]
+rownames(daily_factors) <- NULL
+
+# Save your data_list into an .RData file
+save(daily_factors, file = "Data/daily_factors.RData")
+rm(list = ls())
+
---
+++ RCode/PostPublication/ivol_mom_groups.R
@@ -0,0 +1,98 @@
+load("Data/monthly_ivol.RData")
+load("Data/monthly_factors.RData")
+
+#remove mkt factor
+monthly_factors <- monthly_factors[,setdiff(names(monthly_factors), "mkt")]
+row.names(monthly_factors) <- NULL
+
+#################################################################################
+#IVOL
+#30p/70p
+
+# Create empty dataframe for groups
+ivol_groups <- monthly_ivol
+ivol_groups[, -which(names(ivol_groups) == "date")] <- NA
+
+# Factor columns (exclude date)
+factor_cols <- setdiff(names(monthly_ivol), "date")
+
+# Loop over rows (months)
+for (i in 1:nrow(monthly_ivol)) {
+  
+  # Extract row values
+  row_vals <- as.numeric(monthly_ivol[i, factor_cols])
+  
+  # Compute breakpoints
+  p30 <- quantile(row_vals, probs = 0.3, na.rm = TRUE)
+  p70 <- quantile(row_vals, probs = 0.7, na.rm = TRUE)
+  
+  # Assign S / M / L
+  groups <- ifelse(row_vals <= p30, "IV1",
+                   ifelse(row_vals <= p70, "IV2", "IV3"))
+  
+  # Store result
+  ivol_groups[i, factor_cols] <- as.list(groups)
+}
+
+save(ivol_groups, file = "Data/ivol_groups.RData")
+
+###########################################################################
+#MOM
+factor_cols <- setdiff(names(monthly_factors), "date")
+
+# Keep date column, set others to NA
+mom_groups <- monthly_factors[, c("date", factor_cols)]
+mom_groups[, factor_cols] <- NA
+mom_groups <- mom_groups[-(1:12),]
+row.names(mom_groups) <- NULL
+
+for (i in 1:nrow(monthly_factors)-12) {
+  
+# Current month values
+#monthly_factor i+11 = 1992-04-30 (momentum is lagged return)
+#ivol_groups i = 1992-05-31
+mom_vals <- as.numeric(monthly_factors[i+11, factor_cols])
+ivol_grp <- as.character(ivol_groups[i, factor_cols])
+
+# Initialize MOM row
+mom_row <- rep(NA, length(mom_vals))
+
+for (iv in c("IV1","IV2","IV3")) {
+  
+  # Indices for this IVOL group
+  idx_iv <- which(ivol_grp == iv)
+  
+  if (length(idx_iv) > 0) {
+    # MOM values within IVOL group
+    mom_subset <- mom_vals[idx_iv]
+    
+    # Compute 30/70 breakpoints within IVOL group
+    p30 <- quantile(mom_subset, 0.3, na.rm = TRUE)
+    p70 <- quantile(mom_subset, 0.7, na.rm = TRUE)
+    
+    # Assign MOM groups
+    mom_row[idx_iv] <- ifelse(mom_subset <= p30, "M1",
+                              ifelse(mom_subset <= p70, "M2", "M3"))
+  }
+}
+  # Store dependent MOM assignment in factor columns only
+mom_groups[i, factor_cols] <- mom_row
+}
+
+save(mom_groups, file = "Data/mom_groups.RData")
+
+
+###########################################################################
+#Test
+
+# Create table counts first
+tab <- table(
+  IVOL = as.vector(as.matrix(ivol_groups[, factor_cols])),
+  MOM  = as.vector(as.matrix(mom_groups[, factor_cols]))
+)
+
+# Convert to percentages within each IVOL row
+tab_percent <- prop.table(tab, margin = 1) * 100
+
+# View results
+tab_percent
